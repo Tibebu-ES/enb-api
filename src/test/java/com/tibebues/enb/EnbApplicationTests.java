fb9b161ec6f2ad451511b3ffb85fec41b2bbc3a6
Tibebu-ES <tibebuenyew@gmail.com>
2020-12-05 15:40:13 +0300

    First Commit: Enviroment setup done for Spring boot

---
+++ src/test/java/com/tibebues/enb/EnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tibebues.enb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
